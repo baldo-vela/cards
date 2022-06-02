91f45dbfe2e1995c23f100090bd6f1eadcd2a01d
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-06-02 05:15:38 -0500

    Improves Documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,16 +1,10 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+    Returns a list of strings representing a deck of playing cards.
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -31,9 +25,18 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Five of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
-
   end
 
   def save(deck, filename) do
